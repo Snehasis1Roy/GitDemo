e1431dcfeedfd2b41939531fa7c2f94e5466516f
Snehasis1Roy <snehasisroy2002@gamil.com>
2025-05-24 17:13:18 +0530

    Login in crediantial 1

---
+++ src/LoginCredintial.java
@@ -0,0 +1,9 @@
+public class LoginCredintial {
+    public static void main(String[] args) {
+        System.out.println("you should provide some information ");
+        int i=0;
+        if(i==1){
+            System.out.println("accepted");
+        }
+    }
+}
